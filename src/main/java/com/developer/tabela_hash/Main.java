d6847c247c40e7d6893037c8ef00dfc1fb7aa86a
JoaoVLima <celu.joaolima@gmail.com>
2025-10-04 10:58:25 -0300

    init

---
+++ src/main/java/com/developer/tabela_hash/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.developer.tabela_hash;
+
+/**
+ *
+ * @author Lima
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Tabela_HASH tabela = new Tabela_HASH();
+    }
+}
---
+++ src/main/java/com/developer/tabela_hash/Tabela_HASH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.developer.tabela_hash;
+
+/**
+ *
+ * @author Lima
+ */
+public class Tabela_HASH {
+
+    public Tabela_HASH(){
+        System.out.println("Tabelao");
+    }
+}
